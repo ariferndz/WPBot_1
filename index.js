af5fe71debd79df58c931e9fad5f95794c86d5b2
ariferndz <129589869+ariferndz@users.noreply.github.com>
2025-02-05 16:40:21 +0000

    Agregar archivos faltantes

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+require('dotenv').config();
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en puerto ${PORT}`);
+});
